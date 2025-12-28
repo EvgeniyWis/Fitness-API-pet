c6189348f9fbf393f95de2a8eba3e2eaad50ebc9
EvgeniyWis <kworkEvgeniy@yandex.ru>
2025-12-28 16:54:16 +0300

    update (getting workouts): add filters, clean code.

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 from fastapi import FastAPI
-from models import Workout, WorkoutCreate
+from models import Workout, WorkoutCreate, GymType
+from datetime import date
+from helpers.filters import filter_workouts
 
 app = FastAPI()
 
-workout_list = [
-    Workout(id=1, type="gym", duration=30, repetitions=10),
-    Workout(id=2, type="volleyball", duration=60, repetitions=20),
-]
+workout_list = []
 
 
 @app.post("/workouts")
 async def create_workout(workout_data: WorkoutCreate) -> Workout:
     workout = Workout(
         id=len(workout_list) + 1,
-        type=workout_data.type,
-        duration=workout_data.duration,
-        repetitions=workout_data.repetitions
+        **workout_data.model_dump()
     )
     workout_list.append(workout)
     return workout
 
 
 @app.get("/workouts")
-async def get_workouts() -> list[Workout]:
-    return workout_list
+async def get_workouts(type: GymType | None = None, date_from: date | None = None, date_to: date | None = None, min_duration: int | None = None, max_duration: int | None = None) -> list[Workout]:
+    return filter_workouts(
+        workout_list,
+        type=type,
+        date_from=date_from,
+        date_to=date_to,
+        min_duration=min_duration,
+        max_duration=max_duration,
+    )
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from typing import Literal
-
+from datetime import date
 from pydantic import BaseModel
+from typing import Optional
 
 GymType = Literal["gym", "volleyball"]
 
@@ -8,6 +9,9 @@ class WorkoutCreate(BaseModel):
     type: GymType
     duration: int
     repetitions: int
+    planned_date: Optional[date] = None
+    notes: Optional[str] = None
+    exercises: Optional[list[str]] = None
 
 class Workout(WorkoutCreate):
     id: int
