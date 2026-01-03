7782c5345eac3175adac9e53744393bcf5e7e2ab
EvgeniyWis <kworkEvgeniy@yandex.ru>
2026-01-03 17:10:25 +0300

    feat (protected routes): add role for user.

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,22 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field
+from enum import Enum
+import sqlalchemy as sa
+
+
+class UserRole(str, Enum):
+    ADMIN = "admin"
+    USER = "user"
+
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     username: str = Field()
     password: str = Field()
+    role: UserRole = Field(
+        default=UserRole.USER,
+        sa_column=sa.Column(
+            sa.Enum('admin', 'user', name='userrole', native_enum=False),
+            nullable=False
+        )
+    )
