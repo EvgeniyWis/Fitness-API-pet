8b8bb132acdf5bae14f7feacfa68e14587f57e6a
EvgeniyWis <kworkEvgeniy@yandex.ru>
2026-01-03 17:32:29 +0300

    clean code (deps): delete unused function.

--- app/api/deps.py
+++ app/api/deps.py
@@ -1,30 +1,8 @@
 from fastapi import Depends, Request, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer
 from app.security.get_user_id_by_token import get_user_id_by_token
 from app.services.user_service import user_service
 from app.services.jwt_tokens_service import jwt_tokens_service
 from app.models.user import User, UserRole
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    """Получение текущего пользователя по JWT-токену из заголовка Authorization"""
-    # Проверяем не истёк ли Access токен и не инвалидирован ли он
-    if jwt_tokens_service.check_access_token_expired(token):
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Access токен истёк или инвалидирован"
-        )
-    
-    user_id = get_user_id_by_token(token)
-    user = user_service.get_user_by_id(user_id)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail="Пользователь не найден"
-        )
-    return user
-
 
 async def get_current_user_from_cookie(request: Request):
     """Получение текущего пользователя по JWT-токену из куки"""
